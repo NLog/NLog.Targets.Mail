56a5a0512efbfd141a01ad6ec2f7b60af53630be
Michael Baird <mike@baird.rocks>
2016-03-03 14:44:11 -0600

    using statements not inside preprocessor directive

--- tests/NLog.UnitTests/Targets/MailTargetTests.cs
+++ tests/NLog.UnitTests/Targets/MailTargetTests.cs
@@ -44,7 +44,7 @@ namespace NLog.UnitTests.Targets
     using NLog.Layouts;
     using NLog.Targets;
     using Xunit;
-	using System.IO;
+	  using System.IO;
 
     public class MailTargetTests : NLogTestBase
     {
